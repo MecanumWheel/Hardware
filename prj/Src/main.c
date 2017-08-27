682e42b0651b0fb55e30007694cee96f635584fe
kb4 <kent_brockman4@mail.ru>
2017-08-27 21:31:37 +0700

    CRC16 OK

--- prj/Src/main.c
+++ prj/Src/main.c
@@ -259,44 +259,21 @@ static void MX_GPIO_Init(void)
 
 }
 
-/* USER CODE BEGIN 4 */
-
-#define POLY 0x8408
-/*
-//                                      16   12   5
-// this is the CCITT CRC 16 polynomial X  + X  + X  + 1.
-// This works out to be 0x1021, but the way the algorithm works
-// lets us use 0x8408 (the reverse of the bit pattern).  The high
-// bit is always assumed to be set, thus we only use 16 bits to
-// represent the 17 bit value.
-*/
-
 uint16_t crc16(uint8_t *data_p, uint16_t length)
 {
-      uint8_t i;
-      uint16_t data;
-      uint16_t crc = 0xffff;
-
-      if (length == 0)
-            return (~crc);
-
-      do
-      {
-            for (i=0, data=(uint16_t)0xff & *data_p++;
-                 i < 8; 
-                 i++, data >>= 1)
-            {
-                  if ((crc & 0x0001) ^ (data & 0x0001))
-                        crc = (crc >> 1) ^ POLY;
-                  else  crc >>= 1;
-            }
-      } while (--length);
-
-      crc = ~crc;
-      data = crc;
-      crc = (crc << 8) | (data >> 8 & 0xff);
-
-      return (crc);
+	uint16_t crc = 0;
+	for (uint16_t i=0; i<length; i++)
+	{
+		crc = crc ^ (data_p[i]<<8);
+		for (uint16_t j=0; j<8; j++)
+		{
+			if((crc & 0x8000) != 0)
+				crc = (crc << 1) ^ 0x1021;
+			else
+				crc <<= 1;
+		}
+	}
+	return crc;
 }
 
 void CalcCRC(uint8_t* message)
